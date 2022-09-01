257ac6800ffe87ccb05b41fef5bb582ce410ec00
fredrikb12 <fredrik.backman12@gmail.com>
2022-09-01 17:04:31 +0200

    Add prettier and eslint to project

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import dotenv from "dotenv";
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { Request, Response } from "express";
-import { router } from "./routes/start.js";
-import loggerMiddleware from "./middleware/logger.js";
+import { Request, Response } from 'express';
+import { router } from './routes/start.js';
+import loggerMiddleware from './middleware/logger.js';
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", router);
+app.use('/', router);
 
-app.post("/", (req: Request, res: Response) => {
+app.post('/', (req: Request, res: Response) => {
   res.send(req.body);
 });
 
-app.listen(process.env.PORT || "3000", () => {
+app.listen(process.env.PORT || '3000', () => {
   console.log(`Listening to ${process.env.PORT || 3000}`);
 });
 
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
   console.log(`${req.method} ${req.path}`);
